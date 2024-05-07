74466b710dec2fe0820c9d464cfcdcf518a3cc39
AustineRai <146161799+AustineRai@users.noreply.github.com>
2024-05-07 18:13:34 +0800

    Add files via upload

---
+++ java.js
@@ -0,0 +1,15 @@
+var backgroundColors = ['#ffcccc', '#ccffcc'];
+var currentColorIndex = 0;
+
+function changeImage() {
+    var image1 = document.getElementById('image1');
+    var image2 = document.getElementById('image2');
+    var currentImage = image1.classList.contains('d-none') ? image2 : image1;
+
+    image1.classList.toggle('d-none');
+    image2.classList.toggle('d-none');
+    currentImage.style.display = 'block';
+
+    document.body.style.backgroundColor = backgroundColors[currentColorIndex];
+    currentColorIndex = (currentColorIndex + 1) % backgroundColors.length;
+}
